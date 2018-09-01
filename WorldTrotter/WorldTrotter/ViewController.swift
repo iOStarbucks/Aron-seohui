2a3880e7f0398c98676406ec9b9e9db669d83530
seohui <seohui3927@gmail.com>
2018-09-01 15:38:28 +0900

    [chapter3]view and view hierarchy

---
+++ WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by seohui on 15/08/2018.
+//  Copyright © 2018 seohui. All rights reserved.
+//
+
+import UIKit
+
+// root level window
+class ViewController: UIViewController {
+    
+    
+}
+
